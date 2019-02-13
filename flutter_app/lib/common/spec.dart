a2fe10eebbefebf5650969fabe88f9b804cca39f
GuHaijun <243297288@qq.com>
2019-02-13 18:35:47 +0800

    #modify 修改version位置

--- flutter_app/lib/common/spec.dart
+++ flutter_app/lib/common/spec.dart
@@ -2,23 +2,22 @@
 class ProgramSpec {
   const ProgramSpec({
     this.name,
+    this.version,
     this.author,
     this.description,
     this.iconUrl,
-    this.version,
     this.images = const [""],
     this.flutterAssertUrl,
     this.github,
     this.feature = "",
     this.versionRecord = const [""],
-
   });
 
   final String name;
+  final String version;
   final String author;
   final String description;
   final String iconUrl;
-  final String version;
   final List<String> images;
   final String flutterAssertUrl;
   final String github;
